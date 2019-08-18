d2a2b2cbfa9af287c04b107f6be8d9e7eb7decb4
rsoriano <rsorianosanchis@gmail.com>
2019-08-18 19:58:07 +0200

    third commit

--- app.js
+++ app.js
@@ -5,10 +5,16 @@ console.log(argv.adress);
 //
 getSpotData(argv.adress)
     .then(resp => {
-        console.log(resp);
+        //console.log(resp);
         return getClimaData(resp.lat, resp.lon)
     })
     .then(resp => {
-        console.log(resp);
+        //console.log(resp);
+        console.log(`The temperature in ${resp.name} is ${resp.main.temp}`);
+
     })
-    .catch(err => { console.log(err) });
+    .catch(err => {
+        console.log(`no se pudo determinar el clima para ${argv.adress}`);
+
+        console.log(err)
+    });
